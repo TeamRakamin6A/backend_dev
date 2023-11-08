34666586e962d71aa46abe841d51050d9921aae4
ranggakrisna <infinity.duos@gmail.com>
2023-11-08 20:56:44 +0700

    fixing authmiddleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const UserRouter = require("./UserRoute");
+const UsersController = require("../controllers/UserController");
+const authMiddleware = require("../middlewares/AuthMiddleware");
+
+router.post("/api/users/login", UsersController.loginUser);
+router.post("/api/users/register", UsersController.registerUser);
+
+router.use(authMiddleware)
 
 router.use("/api/users", UserRouter);
 
