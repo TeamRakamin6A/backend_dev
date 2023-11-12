ba7c5d37174e39d9a010dff15904acdfe43f6989
ranggakrisna <infinity.duos@gmail.com>
2023-11-12 18:39:47 +0700

    add more code

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,30 +1,40 @@
-const createOrder = (req, res, next) => {
-  try {
-    const {
-      invoice,
-      total_price,
-      warehouse_id,
-      customer_id,
-      status,
-      order_item_ids,
-    } = req.body;
-  } catch (error) {
-    next(error);
-  }
+const { Order } = require("../models");
+
+const createOrder = async (req, res, next) => {
+    try {
+        const {
+            invoice,
+            total_price,
+            warehouse_id,
+            customer_id,
+            status,
+            order_item_ids,
+        } = req.body;
+
+        const order = await Order.Create({
+            invoice,
+            total_price,
+            warehouse_id,
+            customer_id,
+            status,
+        }, { returning: true });
+    } catch (error) {
+        next(error);
+    }
 };
 
-const getAllOrder = (req, res, next) => {};
+const getAllOrder = (req, res, next) => { };
 
-const getOrderById = (req, res, next) => {};
+const getOrderById = (req, res, next) => { };
 
-const updateOrder = (req, res, next) => {};
+const updateOrder = (req, res, next) => { };
 
-const deleteOrder = (req, res, next) => {};
+const deleteOrder = (req, res, next) => { };
 
 module.exports = {
-  createOrder,
-  getAllOrder,
-  getOrderById,
-  updateOrder,
-  deleteOrder,
+    createOrder,
+    getAllOrder,
+    getOrderById,
+    updateOrder,
+    deleteOrder,
 };
