1805e6a3fde20bc775dc5195ed6622cbc422ef76
ranggakrisnaa <infinity.duos@gmail.com>
2023-11-15 11:33:51 +0700

    completed endpoint dashboard
    change response to plural and change seeder field value table order
    
    change response to plural and change seeder field value table order
    
    change response to plural and change seeder field value table order

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -1,8 +1,29 @@
+const { Order, Supply_Order, Warehouse, Item, Customer } = require("../models")
+
 const showDashboard = async (req, res, next) => {
     try {
 
-    } catch (error) {
+        let totalRevenues = 0
+        let totalExpenses = 0
 
+        const foundRevenue = await Order.findAll()
+        foundRevenue.map((data) => totalRevenues += data.total_price)
+
+        const foundExpenses = await Supply_Order.findAll()
+        foundExpenses.map((data) => totalExpenses += data.total_price)
+
+        const { count: totalWarehouses, } = await Warehouse.findAndCountAll()
+
+        const { count: totalItems } = await Item.findAndCountAll()
+
+        const { count: totalCustomers } = await Customer.findAndCountAll()
+
+        const { count: totalOrders } = await Order.findAndCountAll()
+
+        res.status(200).json({ status: true, total_revenues: totalRevenues, total_expenses: totalExpenses, total_warehouses: totalWarehouses, total_products: totalItems, total_customers: totalCustomers, total_orders: totalOrders })
+
+    } catch (error) {
+        next(error)
     }
 
 }
